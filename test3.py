9509dd6f88a38283c974df356bbdb0bc41c80d42
IPvZero <ipvzero@gmail.com>
2020-01-31 20:09:11 +0000

    This is my very first ever commit!

---
+++ test3.py
@@ -0,0 +1,18 @@
+from nornir import InitNornir
+from nornir.plugins.functions.text import print_result, print_title
+from nornir.plugins.tasks.networking import netmiko_send_config, netmiko_send_command
+
+def baseconfig(ipvzero):
+    ipvzero.run(task=netmiko_send_config, config_file= "config_text")
+    ipvzero.run(task=netmiko_send_command, command_string = "show run | sec user")
+
+nr = InitNornir(
+    config_file="config.yaml", dry_run=True
+)
+
+devices = nr.filter(asn=123)
+results = devices.run(task = baseconfig)
+
+print_title("DEPLOYING AUTOMATED BASELINE CONFIGURATIONS")
+print_result(results)
+
