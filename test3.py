af61d840ddbf1bfdfbc6df49a06ee52d845e83bc
IPvZero <ipvzero@gmail.com>
2020-01-31 20:30:06 +0000

    This is my second commit - John

--- test3.py
+++ test3.py
@@ -1,7 +1,7 @@
 from nornir import InitNornir
 from nornir.plugins.functions.text import print_result, print_title
 from nornir.plugins.tasks.networking import netmiko_send_config, netmiko_send_command
-
+#THIS IS JUST A COMMENT FOR PURPOSES OF TESTING@:
 def baseconfig(ipvzero):
     ipvzero.run(task=netmiko_send_config, config_file= "config_text")
     ipvzero.run(task=netmiko_send_command, command_string = "show run | sec user")
